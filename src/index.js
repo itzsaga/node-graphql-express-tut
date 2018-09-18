21283a66a1ac94a6a5e719a78a2713e080979e96
Seth <sethalexander@pm.me>
2018-09-18 12:16:11 -0500

    Base server setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import "dotenv/config";
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer, gql } from "apollo-server-express";
 
-console.log("Hello :-)");
+const app = express();
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: "Seth"
+      };
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers
+});
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+app.listen({ port: 8000 }, () => {
+  console.log("Apollo Server on http://localhost:8000/graphql");
+});
